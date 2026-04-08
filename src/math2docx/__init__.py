070c9b8c189bd31e2f45ba7bc17f357b08ab33de
Николай М <keh9mark@yandex.ru>
2026-04-08 12:48:38 +0300

    feat: add italic\roman font

--- src/math2docx/__init__.py
+++ src/math2docx/__init__.py
@@ -5,8 +5,11 @@ import latex2mathml.converter
 from docx.oxml import parse_xml
 
 
-def _formula(latex_string: str) -> typing.Any:
+def _formula(latex_string: str, is_italic: bool = True) -> typing.Any:
     mathml_output = latex2mathml.converter.convert(latex_string)
+    if not is_italic:
+            mathml_output = mathml_output.replace("<mi>", "<mtext>")
+            mathml_output = mathml_output.replace("</mi>", "</mtext>")
     omml_output = mathml2omml.convert(mathml_output)
     xml_output = (
         f'<p xmlns:m="http://schemas.openxmlformats.org/officeDocument'
@@ -15,13 +18,13 @@ def _formula(latex_string: str) -> typing.Any:
     return parse_xml(xml_output)[0]
 
 
-def add_math(p, latex_string) -> None:
+def add_math(p, latex_string: str, is_italic: bool = True) -> None:
     """Add equation to Paragraph element"""
-    p._p.append(_formula(latex_string))
+    p._p.append(_formula(latex_string, is_italic))
 
 
-def add_math_to_tc(tc, latex_string) -> None:
+def add_math_to_tc(tc, latex_string: str, is_italic: bool = True) -> None:
     """Add equation to Table inner _Cell element"""
     tc._tc.clear_content()
     p = tc._tc.add_p()
-    p.append(_formula(latex_string))
+    p.append(_formula(latex_string, is_italic))
--- src/test/example.py
+++ src/test/example.py
@@ -1,5 +1,6 @@
-import math2docx
 from docx import Document
+
+import math2docx
 
 document = Document()
 p = document.add_paragraph()
@@ -8,4 +9,7 @@ p = document.add_paragraph()
 latex_ = r"\cos (2\theta) = \cos^2 \theta - \sin^2 \theta"
 
 math2docx.add_math(p, latex_ )
+
+p2 = document.add_paragraph()
+math2docx.add_math(p2, latex_, False)
 document.save("new_file.docx")
